e39a2dc1927a34fae63bc1fa71c2ddbe9e7b9168
SimovDenis <131714121+SimovDenis@users.noreply.github.com>
2023-05-20 09:50:44 +0200

    Vjezbanje_insert_naredbe

--- Taksi_sluzba.sql
+++ Taksi_sluzba.sql
@@ -25,7 +25,7 @@ create table vozac(
 
 create table voznja(
     sifra int not null primary key auto_increment,
-    udaljenost int, 
+    udaljenost decimal(3,1), 
     trajanje int,
     cijena int,
     ocjena int
@@ -54,5 +54,40 @@ alter table vozac add foreign key (osoba) references osoba(sifra);
 alter table putnik add foreign key (osoba) references osoba(sifra);
 alter table klijent add foreign key (putnik) references putnik(sifra);
 alter table klijent add foreign key (voznja) references voznja(sifra);
+
+insert into osoba (sifra, ime, prezime) values
+(null, 'Barbara', 'Glović'),
+(null, 'Tina', 'Vukov'),
+(null, 'Bruno', 'Milić');
+
+insert into tvrtka (sifra, naziv, vlasnik, adresa) values
+(null, 'Brzi i žestoki', 'Vin Benzin', null),
+(null, 'Puma', 'Usain Bolt', null),
+(null, 'Europa', 'Ivan Goranić', null);
+
+insert into vozilo (sifra, tip, tvrtka, boja) values
+(null, 'Škoda Octavia', 1, 'Plava'),
+(null, 'Ford Focus', 2, 'Crvena'),
+(null, 'Volvo D40', 3, 'Žuta');
+
+insert into putnik (sifra, osoba) values
+(null, 1),
+(null, 3),
+(null, 2);
+
+insert into voznja (sifra, udaljenost, trajanje, cijena, ocjena) values
+(null, 20, 20, 10, 5),
+(null, 3.5, 10, 10, 3),
+(null, 6, 12, 11, 5);
+
+insert into vozac (sifra, vozilo, putnik, osoba) values
+(null, 2, 3, 1),
+(null, 1, 2, 3),
+(null, 3, 2, 1);
+
+insert into klijent (putnik, voznja) values
+(2, 3),
+(1, 1),
+(3, 3);
 
 
