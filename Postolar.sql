a09f85783dbbf498137d5c76314cf0dcb6b84a6f
SimovDenis <131714121+SimovDenis@users.noreply.github.com>
2023-05-22 11:31:53 +0200

    Vjezba_insert_naredbe

--- Postolar.sql
+++ Postolar.sql
@@ -55,4 +55,34 @@ alter table obuca add foreign key (korisnik) references korisnik(sifra);
 alter table korisnik add foreign key (osoba) references osoba(sifra);
 alter table segrt add foreign key (osoba) references osoba(sifra);
 
+insert into osoba (sifra, ime, prezime, oib) values
+(null, 'Josip', 'Šobić', 64622497144),
+(null, 'Barbara', 'Milić', 24622497157),
+(null, 'Martin', 'Mužar', 54665997144);
+
+insert into korisnik (sifra, osoba) values
+(null, 3),
+(null, 2),
+(null, 1);
+
+insert into postolar (sifra, osoba, iban, naziv_obrta) values
+(null, 1, null, 'Patika'),
+(null, 2, null, 'Moja Štikla'),
+(null, 3, null, 'Korak');
+
+insert into segrt (sifra, osoba, datum_pocetka_rada, broj_ugovora) values
+(null, 1, '2019-06-07', '1456-2019'),
+(null, 2, '2012-03-28', '9987-2012'),
+(null, 3, '2020-12-01', '4655-2020');
+
+insert into obuca (sifra, naziv, boja, starost, korisnik) values
+(null, 'Nike Air', 'Bijelo-Crvena', 20, 2),
+(null, 'Reebok', 'Žuta', 2, 3),
+(null, 'Adidas', 'Crna', 1, 2);
+
+insert into popravak (sifra, vrsta_popravka, postolar, segrt, obuca, cijena) values
+(null, 'Bojanje', 1, null, 2, 50.55),
+(null, 'Krpanje', null, 2, 1, 18.45),
+(null, 'Zamjena konca na đonu', null, 3, 3, 10);
+
 
