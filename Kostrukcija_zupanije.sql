1ee01e4b8b33330ea0458cb9f5c6892a04d9c26b
SimovDenis <131714121+SimovDenis@users.noreply.github.com>
2023-05-30 10:26:05 +0200

    Urednija_insert_naredba

--- Kostrukcija_zupanije.sql
+++ Kostrukcija_zupanije.sql
@@ -1,3 +1,5 @@
+# c:\xampp\mysql\bin\mysql -uroot --default_character_set=utf8 < D:\GitHub\Edunova\Kostrukcija_zupanije.sql
+
 drop database if exists konstrukcija_zupanije;
 create database konstrukcija_zupanije;
 use konstrukcija_zupanije;
@@ -34,73 +36,79 @@ alter table mjesto add foreign key (opcina) references opcina(sifra);
 insert into zupan(ime, prezime) values ('Ivan', 'Anušić');
 insert into zupanija(naziv, zupan) values ('Osječko-Baranjska', 1);
 
-insert into opcina(zupanija, naziv) values (1, 'Belišće');
-insert into opcina(zupanija, naziv) values (1, 'Valpovo');
-insert into opcina(zupanija, naziv) values (1, 'Petrijevci');
-insert into opcina(zupanija, naziv) values (1, 'Vuka');
-insert into opcina(zupanija, naziv) values (1, 'Čepin');
-insert into opcina(zupanija, naziv) values (1, 'Bilje');
+insert into opcina(zupanija, naziv) values 
+(1, 'Belišće'),
+(1, 'Valpovo'),
+(1, 'Petrijevci'),
+(1, 'Vuka'),
+(1, 'Čepin'),
+(1, 'Bilje');
 
-insert into mjesto(opcina, naziv) values(1, 'Belišće');
-insert into mjesto(opcina, naziv) values(1, 'Tiborjanci');
-insert into mjesto(opcina, naziv) values(1, 'Gat');
-insert into mjesto(opcina, naziv) values(2, 'Valpovo');
-insert into mjesto(opcina, naziv) values(2, 'Šag');
-insert into mjesto(opcina, naziv) values(2, 'Marjančaci');
-insert into mjesto(opcina, naziv) values(3, 'Petrijevci');
-insert into mjesto(opcina, naziv) values(3, 'Satnica');
-insert into mjesto(opcina, naziv) values(4, 'Vuka');
-insert into mjesto(opcina, naziv) values(4, 'Hrastovac');
-insert into mjesto(opcina, naziv) values(5, 'Čepin');
-insert into mjesto(opcina, naziv) values(6, 'Bilje');
+insert into mjesto(opcina, naziv) values
+(1, 'Belišće'),
+(1, 'Tiborjanci'),
+(1, 'Gat'),
+(2, 'Valpovo'),
+(2, 'Šag'),
+(2, 'Marjančaci'),
+(3, 'Petrijevci'),
+(3, 'Satnica'),
+(4, 'Vuka'),
+(4, 'Hrastovac'),
+(5, 'Čepin'),
+(6, 'Bilje');
 
 # Grad Zagreb
 insert into zupan(ime, prezime) values ('Tomislav', 'Tomašević');
 insert into zupanija(naziv, zupan) values ('Grad Zagreb', 2);
 
-insert into opcina(zupanija, naziv) values (2, 'Bistra');
-insert into opcina(zupanija, naziv) values (2, 'Brckovljani');
-insert into opcina(zupanija, naziv) values (2, 'Brdovec');
-insert into opcina(zupanija, naziv) values (2, 'Dubravica');
-insert into opcina(zupanija, naziv) values (2, 'Jakovlje');
-insert into opcina(zupanija, naziv) values (2, 'Kravarsko');
+insert into opcina(zupanija, naziv) values 
+(2, 'Bistra'),
+(2, 'Brckovljani'),
+(2, 'Brdovec'),
+(2, 'Dubravica'),
+(2, 'Jakovlje'),
+(2, 'Kravarsko');
 
-insert into mjesto(opcina, naziv) values(7, 'Bukovje Bistransko');
-insert into mjesto(opcina, naziv) values(7, 'Donja Bistra');
-insert into mjesto(opcina, naziv) values(7, 'Gornja Bistra');
-insert into mjesto(opcina, naziv) values(8, 'Brckovljani');
-insert into mjesto(opcina, naziv) values(8, 'Donje Dvorišće');
-insert into mjesto(opcina, naziv) values(8, 'Gornje Dvorišće');
-insert into mjesto(opcina, naziv) values(8, 'Gračec');
-insert into mjesto(opcina, naziv) values(9, 'Brdovec');
-insert into mjesto(opcina, naziv) values(9, 'Donji Laduč');
-insert into mjesto(opcina, naziv) values(10, 'Dubravica');
-insert into mjesto(opcina, naziv) values(11, 'Igrišće');
-insert into mjesto(opcina, naziv) values(12, 'Čakanec');
+insert into mjesto(opcina, naziv) values
+(7, 'Bukovje Bistransko'),
+(7, 'Donja Bistra'),
+(7, 'Gornja Bistra'),
+(8, 'Brckovljani'),
+(8, 'Donje Dvorišće'),
+(8, 'Gornje Dvorišće'),
+(8, 'Gračec'),
+(9, 'Brdovec'),
+(9, 'Donji Laduč'),
+(10, 'Dubravica'),
+(11, 'Igrišće'),
+(12, 'Čakanec');
 
 # Vukovarsko-Srijemska
 insert into zupan(ime, prezime) values ('Damir', 'Dekanić');
 insert into zupanija(naziv, zupan) values ('Vukovarsko-Srijemska', 3);
 
-insert into opcina(zupanija, naziv) values (3, 'Borovo');
-insert into opcina(zupanija, naziv) values (3, 'Gunja');
-insert into opcina(zupanija, naziv) values (3, 'Trpinja');
-insert into opcina(zupanija, naziv) values (3, 'Andrijaševci');
-insert into opcina(zupanija, naziv) values (3, 'Babina Greda');
-insert into opcina(zupanija, naziv) values (3, 'Tovarnik');
+insert into opcina(zupanija, naziv) values 
+(3, 'Borovo'),
+(3, 'Gunja'),
+(3, 'Trpinja'),
+(3, 'Andrijaševci'),
+(3, 'Babina Greda'),
+(3, 'Tovarnik');
 
-insert into mjesto(opcina, naziv) values(13, 'Borovo');
-insert into mjesto(opcina, naziv) values(14, 'Gunja');
-insert into mjesto(opcina, naziv) values(15, 'Bobota');
-insert into mjesto(opcina, naziv) values(15, 'Trpinja');
-insert into mjesto(opcina, naziv) values(15, 'Vera');
-insert into mjesto(opcina, naziv) values(15, 'Ćelije');
-insert into mjesto(opcina, naziv) values(15, 'Pačetin');
-insert into mjesto(opcina, naziv) values(16, 'Rokovci');
-insert into mjesto(opcina, naziv) values(16, 'Andrijaševci');
-insert into mjesto(opcina, naziv) values(17, 'Babina Greda');
-insert into mjesto(opcina, naziv) values(18, 'Ilača');
-insert into mjesto(opcina, naziv) values(18, 'Tovarnik');
+insert into mjesto(opcina, naziv) values
+(13, 'Borovo'),
+(14, 'Gunja'),
+(15, 'Bobota'),
+(15, 'Trpinja'),
+(15, 'Vera'),
+(15, 'Ćelije'),
+(15, 'Pačetin'),
+(16, 'Rokovci'),
+(16, 'Andrijaševci'),
+(17, 'Babina Greda'),
+(18, 'Ilača'),
+(18, 'Tovarnik');
 
 # Promjena imena mjesta i brisanje općina
 update mjesto set naziv = 'Kitišanci' where naziv = 'Belišće';
