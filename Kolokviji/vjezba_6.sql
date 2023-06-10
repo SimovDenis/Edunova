98de9f22663e53871fa5f101cc376366a63f4698
SimovDenis <131714121+SimovDenis@users.noreply.github.com>
2023-06-10 10:59:59 +0200

    select_na_vise_tablica

--- Kolokviji/vjezba_6.sql
+++ Kolokviji/vjezba_6.sql
@@ -110,4 +110,14 @@ select narukvica
 from brat
 where treciputa is null;
 
+select a.drugiputa, f.zena, e.narukvica
+from ostavljena a
+inner join prijatelj_ostavljena b on a.sifra = b.ostavljena
+inner join prijatelj c on b.prijatelj = c.sifra
+inner join brat d on c.sifra = d.prijatelj
+inner join zena e on b.sifra = e.brat
+inner join decko f on e.sifra = f.zena
+where d.treciputa is not null and c.prstena = 219
+order by e.narukvica desc;
+
 
