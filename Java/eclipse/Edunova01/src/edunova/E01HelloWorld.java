58a8d619c97339417d6fd99c7b57050974f4286b
SimovDenis <131714121+SimovDenis@users.noreply.github.com>
2023-06-13 12:19:51 +0200

    Promjena_imena_klasi_i_kreiranje_nove_klase

---
+++ Java/eclipse/Edunova01/src/edunova/E01HelloWorld.java
@@ -0,0 +1,14 @@
+package edunova;
+
+public class E01HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+		System.err.println("\tGreška\\");
+		System.out.print("Prvi\n");
+		System.out.print("Drugi\"");
+		
+
+	}
+
+}
