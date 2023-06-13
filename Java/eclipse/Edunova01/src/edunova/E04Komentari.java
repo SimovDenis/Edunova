201b3da2e798816ffce6cd531cc03bd692ee1450
SimovDenis <131714121+SimovDenis@users.noreply.github.com>
2023-06-13 20:07:09 +0200

    Vjezba

---
+++ Java/eclipse/Edunova01/src/edunova/E04Komentari.java
@@ -0,0 +1,40 @@
+package edunova;
+
+//jedna linija
+
+/*
+ * 
+ * Više linija 1. način
+ * 
+ */
+
+/*
+ 
+Više linija 2. način 
+ 
+*/
+
+//Više
+//linija
+//3.način
+
+//posebna vrsta komentara je dokumentacija
+/**
+ * Ovo je moja klasa gdje pokazujem kako se radis s komentarima
+ * @author Denis
+ *
+ */
+public class E04Komentari {
+	//jedna linija
+	/**
+	 *Ovo mi je main metoda 
+	 * @param args
+	 */
+	public static void main(String[] /* i ovo je komentar */ args) {
+		
+		//jedna linija
+		System.out.println("Komentari"); // komentar na kraju linije
+
+	}
+
+}
