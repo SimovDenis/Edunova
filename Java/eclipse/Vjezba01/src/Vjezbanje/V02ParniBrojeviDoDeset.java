f86088983f81ebdb169b33f80574eccb269fc886
SimovDenis <131714121+SimovDenis@users.noreply.github.com>
2023-06-14 09:16:37 +0200

    Izrada_koda

---
+++ Java/eclipse/Vjezba01/src/Vjezbanje/V02ParniBrojeviDoDeset.java
@@ -0,0 +1,17 @@
+package Vjezbanje;
+
+public class V02ParniBrojeviDoDeset {
+
+	public static void main(String[] args) {
+		int niz[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+		
+		for (int i = 1; i < 10; i+=2) {
+						
+			System.out.println(niz[i]);
+			
+		}
+		
+
+	}
+
+}
