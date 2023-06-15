e56edd6ab4467c70c61613963fe6c928eada68f5
SimovDenis <131714121+SimovDenis@users.noreply.github.com>
2023-06-15 09:33:27 +0200

    Rijesen_3._zadatak

---
+++ Java/eclipse/Edunova02/src/edunova/Z03.java
@@ -0,0 +1,15 @@
+package edunova;
+
+import javax.swing.JOptionPane;
+
+public class Z03 {
+
+	public static void main(String[] args) {
+		String grad = JOptionPane.showInputDialog("Unesi grad iz kojeg dolaziš");
+		int godine = Integer.parseInt(JOptionPane.showInputDialog("Unesi svoje godine"));
+		
+		System.out.println("Imam " + godine + "g i dolazim iz mjesta " + grad + ".");
+
+	}
+
+}
