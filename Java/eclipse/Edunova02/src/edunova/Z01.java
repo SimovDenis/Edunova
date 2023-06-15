df21a7c33fc4ed4200f970dac56dd372c8f31f51
SimovDenis <131714121+SimovDenis@users.noreply.github.com>
2023-06-15 09:43:17 +0200

    Rijesen_1._zadatak

---
+++ Java/eclipse/Edunova02/src/edunova/Z01.java
@@ -0,0 +1,19 @@
+package edunova;
+
+public class Z01 {
+
+	public static void main(String[] args) {
+		
+		int broj = 17;
+		float decimalni = 12.99f;
+		char slovo = 'D';
+		boolean istina = true;
+		
+		System.out.println(broj);
+		System.out.println(decimalni);
+		System.out.println(slovo);
+		System.out.println(istina);
+		
+	}
+
+}
