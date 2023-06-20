8b88334e8e07626c49edea639315912aa2c065d2
SimovDenis <131714121+SimovDenis@users.noreply.github.com>
2023-06-20 10:14:07 +0200

    Z02Rjesenje

---
+++ Java/eclipse/Edunova03/src/edunova/Z02.java
@@ -0,0 +1,28 @@
+package edunova;
+
+import javax.swing.JOptionPane;
+
+public class Z02 {
+	public static void main(String[] args) {
+		
+				// Korisnik unosi cijeli broj
+				// Program ispisuje da li je 
+				// paran ili nije
+		
+		int broj = Integer.parseInt(JOptionPane.showInputDialog("Unesi broj za provjeru"));
+		
+		
+		// rješenje zadatka uvjetnim grananjem if
+		if (broj % 2 == 0) {
+			System.out.println("Broj je paran");
+		} else {
+			System.out.println("Broj je neparan");
+		}
+		
+		
+		// inline rješenje zadatka
+		System.out.println("Broj je " + (broj%2 != 0 ? "ne":"") + "paran");
+		
+	}
+
+}
