b00bc1a84f7447436f49cace7a687f7135cc7b3b
SimovDenis <131714121+SimovDenis@users.noreply.github.com>
2023-06-20 11:35:13 +0200

    Z07Rjesenje

---
+++ Java/eclipse/Edunova03/src/edunova/Z07.java
@@ -0,0 +1,25 @@
+package edunova;
+
+import javax.swing.JOptionPane;
+
+public class Z07 {
+
+	public static void main(String[] args) {
+
+		// Program unosi 2 broja
+		// Ako su oba broja parni
+		// ispisuje njihov zbroj
+		// inače ispisuje njihovu razliku
+
+		int prvi = Integer.parseInt(JOptionPane.showInputDialog("Unesi prvi broj"));
+		int drugi = Integer.parseInt(JOptionPane.showInputDialog("Unesi drugi broj"));
+
+		if (prvi % 2 == 0 && drugi % 2 == 0) {
+			System.out.println(prvi + drugi);
+		} else {
+			System.out.println(prvi - drugi);
+		}
+
+	}
+
+}
