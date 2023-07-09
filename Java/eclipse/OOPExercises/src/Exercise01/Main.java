edc38bd2cbb79f3d5e289eb1f3495c334f7442ae
SimovDenis <131714121+SimovDenis@users.noreply.github.com>
2023-07-09 19:50:23 +0200

    Exercise01

---
+++ Java/eclipse/OOPExercises/src/Exercise01/Main.java
@@ -0,0 +1,20 @@
+package Exercise01;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		// Write a Java program to create a class called "Person" with a name and age
+		// attribute.
+		// Create two instances of the "Person" class, set their attributes using the
+		// constructor, and print their name and age.
+
+		Person male = new Person("Ivan", 22);
+		Person female = new Person("Anja", 20);
+
+		System.out.println(male.getName() + " is " + male.getAge() + " years old.");
+		System.out.println(female.getName() + " is " + female.getAge() + " years old.");
+
+	}
+
+}
