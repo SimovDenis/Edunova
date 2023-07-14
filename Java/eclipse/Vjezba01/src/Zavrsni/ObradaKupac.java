4bcac69c8b2dcbd1d604437ffde909cee6da8e47
SimovDenis <131714121+SimovDenis@users.noreply.github.com>
2023-07-14 12:00:41 +0200

    ObradaKupacZavrseno

--- Java/eclipse/Vjezba01/src/Zavrsni/ObradaKupac.java
+++ Java/eclipse/Vjezba01/src/Zavrsni/ObradaKupac.java
@@ -11,6 +11,16 @@ public class ObradaKupac {
 	
 	public ObradaKupac(){
 		kupci = new ArrayList<>();
+		if(Pomocno.dev) {
+			testniPodaci();
+		}
+	}
+
+	private void testniPodaci() {
+		kupci.add(new Kupac(1, "Indira", "Sopnik", "indira2023@gmail.com"));
+		kupci.add(new Kupac(2, "Martin", "Anzek", "01/555-286"));
+		kupci.add(new Kupac(3, "Anita", "Zima", "zima.22@yahoo.com"));
+		
 	}
 
 	public void prikaziIzbornik() {
@@ -36,8 +46,40 @@ public class ObradaKupac {
 			unosKupca();
 			prikaziIzbornik();
 			break;
+			
+		case 3:
+			izmjenaKupca();
+			prikaziIzbornik();
+			break;
+			
+		case 4:
+			brisanjeKupca();
+			prikaziIzbornik();
+			break;
+			
+		case 5:
+			break;
 		
 		}
+		
+	}
+
+	private void brisanjeKupca() {
+		pregledKupaca();
+		int index = Pomocno.unosBroja("Odaberi kupca za brisanje", "Greska", 1, kupci.size());
+		kupci.remove(index-1);
+		
+	}
+
+	private void izmjenaKupca() {
+		pregledKupaca();
+		int index = Pomocno.unosBroja("Odaberi kupca za izmjenu", "Odabrani kupac ne postoji", 1, kupci.size());
+		Kupac k = kupci.get(index - 1);
+		k.setSifra(Pomocno.unosBroja("Unesi šifru kupca(trenutna šifra: " + k.getSifra() + ")", "Greška", 1, Integer.MAX_VALUE));
+		k.setIme(Pomocno.unosString("Unesi ime kupca(trenutno ime kupca: " + k.getIme() + ")", "Greška"));
+		k.setPrezime(Pomocno.unosString("Unesi ime kupca(trenutno prezime kupca: " + k.getPrezime() + ")", "Greška"));
+		k.setKontakt(Pomocno.unosString("Unesi ime kupca(trenutni kontakt kupca: " + k.getKontakt() + ")", "Greška"));
+		
 		
 	}
 
@@ -53,13 +95,20 @@ public class ObradaKupac {
 	}
 
 	private void pregledKupaca() {
+		System.out.println("-----------------");
+		System.out.println("----- Kupci -----");
+		System.out.println("-----------------");
 		if (kupci.isEmpty()) {
 			System.out.println("Nema unesenog kupca");
 		}
 		
+		int b = 1;
 		for(Kupac k : kupci) {
-			System.out.println(k.getIme() + " " + k.getPrezime());
+			
+			System.out.println(b++ + ". " + k.getIme() + " " + k.getPrezime());
 		}
+		
+		System.out.println("-----------------");
 		
 	}	
 
