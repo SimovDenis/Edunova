6b1edaa78d177c14c2bfee789deaa9caa9b47a8f
SimovDenis <131714121+SimovDenis@users.noreply.github.com>
2023-07-19 10:30:56 +0200

    Omoguceno_brisanje_polaznika_iz_grupe

--- Java/eclipse/Edunova09/src/edunova/ObradaGrupa.java
+++ Java/eclipse/Edunova09/src/edunova/ObradaGrupa.java
@@ -28,7 +28,7 @@ public class ObradaGrupa {
 		System.out.println("1. Pregled postojećih grupa");
 		System.out.println("2. Unos nove grupe");
 		System.out.println("3. Promjena postojeće grupe");
-		System.out.println("4. Brisanje postojeće grupe");
+		System.out.println("4. Brisanje postojeće grupe ili polaznika u grupi");
 		System.out.println("5. Povratak na prethodni izbornik");
 		ucitajStavkuIzbornika();
 	}
@@ -49,12 +49,58 @@ public class ObradaGrupa {
 			prikaziIzbornik();
 			break;
 		case 4:
-			brisanjeGrupa();
+			izbornikBrisanje();
 			prikaziIzbornik();
 			break;
+			
 		case 5:
 			break;
 		}
+	}
+
+	private void izbornikBrisanje() {
+		System.out.println("\nIzbornik brisanje");
+		System.out.println("1. Brisanje postojeće grupe");
+		System.out.println("2. Brisanje člana iz grupe");
+		System.out.println("3. Povratak na prethodni izbornik");
+		odaberiStavkuIzbornikaBrisanje();
+	}
+
+	private void odaberiStavkuIzbornikaBrisanje() {
+		switch(Pomocno.unosRasponBroja("Odaberi stavku izbornika brisanje", "Odabir mora biti 1-3", 1, 3)) {
+		case 1:
+			brisanjeGrupa();
+			break;
+			
+		case 2:
+			brisanjePolaznik();
+			break;
+			
+		case 3:
+			break;
+		}
+		
+	}
+
+	private void brisanjePolaznik() {
+		pregledGrupa();
+		int indexGrupe = Pomocno.unosRasponBroja("Odaberi grupu iz koje želiš obrisati polaznika: ", "Greška", 1, grupe.size());
+		Grupa g = grupe.get(indexGrupe - 1);
+		if(g.getPolaznici().isEmpty()) {
+			System.out.println("\nTrenutno nema niti jednog polaznika u grupi");
+		} else {
+			System.out.println("----------------------------");
+			System.out.println("---- Trenutni polaznici ----");
+			System.out.println("----------------------------");
+			int b = 1;
+			for(Polaznik p : g.getPolaznici()) {
+				System.out.println(b++ + ". " + p);
+			}
+			System.out.println("----------------------------");
+			int indexPolaznika = Pomocno.unosRasponBroja("Odaberi polaznika kojeg želiš obrisati: ", "Greška", 1, g.getPolaznici().size());
+			g.getPolaznici().remove(indexPolaznika - 1);
+		}
+
 	}
 
 	private void pregledGrupa() {
@@ -111,36 +157,45 @@ public class ObradaGrupa {
 	}
 
 	private void promjenaGrupa() {
-		pregledGrupa();
-		int index = Pomocno.unosRasponBroja("Odaberi redni broj grupe: ","Nije dobar odabir",1,grupe.size());
-		Grupa g = grupe.get(index-1);
-		g.setSifra(Pomocno.unosRasponBroja("Unesi šifru grupe (" + g.getSifra() + "): " ,"Pozitivan broj",
-				1,Integer.MAX_VALUE));
-		g.setNaziv(Pomocno.unosString("Unesi naziv grupe (" + g.getNaziv() + "): ","Naziv obavezan"));
-		g.setMaxpolaznika(Pomocno.unosRasponBroja("Unesi maksimalno polaznika grupe: ","Pozitivan broj (5-25)",
-				5,25));
-		System.out.println("Trenutni smjer: " + g.getSmjer().getNaziv());
-		g.setSmjer(postaviSmjer());
-		System.out.println("Trenutni predavač: " + g.getPredavac());
-		g.setPredavac(postaviPredavaca());
-		g.setDatumPocetka(Pomocno.unosDatum("Unesi datum početka"));
-		System.out.println("----------------------------");
-		System.out.println("---- Trenutni polaznici ----");
-		System.out.println("----------------------------");
-		int b=1;
-		for(Polaznik p : g.getPolaznici()) {
-			System.out.println(b++ + ". " + p);
+		if(grupe.isEmpty()) {
+			System.out.println("\nTrenutno nije unesena niti jedna grupa");
+		} else {
+			pregledGrupa();
+			int index = Pomocno.unosRasponBroja("Odaberi redni broj grupe: ","Nije dobar odabir",1,grupe.size());
+			Grupa g = grupe.get(index-1);
+			g.setSifra(Pomocno.unosRasponBroja("Unesi šifru grupe (" + g.getSifra() + "): " ,"Pozitivan broj",
+					1,Integer.MAX_VALUE));
+			g.setNaziv(Pomocno.unosString("Unesi naziv grupe (" + g.getNaziv() + "): ","Naziv obavezan"));
+			g.setMaxpolaznika(Pomocno.unosRasponBroja("Unesi maksimalno polaznika grupe: ","Pozitivan broj (5-25)",
+					5,25));
+			System.out.println("Trenutni smjer: " + g.getSmjer().getNaziv());
+			g.setSmjer(postaviSmjer());
+			System.out.println("Trenutni predavač: " + g.getPredavac());
+			g.setPredavac(postaviPredavaca());
+			g.setDatumPocetka(Pomocno.unosDatum("Unesi datum početka"));
+			System.out.println("----------------------------");
+			System.out.println("---- Trenutni polaznici ----");
+			System.out.println("----------------------------");
+			int b=1;
+			for(Polaznik p : g.getPolaznici()) {
+				System.out.println(b++ + ". " + p);
+			}
+			System.out.println("-------------------");
+			g.setPolaznici(postaviPolaznike());
 		}
-		System.out.println("-------------------");
-		g.setPolaznici(postaviPolaznike());
+
 		
 	}
 
 
 	private void brisanjeGrupa() {
-		pregledGrupa();
-		int index = Pomocno.unosRasponBroja("Odaberi redni broj grupe: ","Nije dobar odabir",1,grupe.size());
-		grupe.remove(index-1);
+		if(grupe.isEmpty()) {
+			System.out.println("\nTrenutno nije unesena niti jedna grupa");
+		} else {
+			pregledGrupa();
+			int index = Pomocno.unosRasponBroja("Odaberi redni broj grupe: ","Nije dobar odabir",1,grupe.size());
+			grupe.remove(index-1);
+		}	
 		
 	}
 
