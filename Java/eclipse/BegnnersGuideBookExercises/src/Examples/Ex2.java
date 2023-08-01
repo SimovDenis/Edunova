4d2ffe72834c25184c480364208b695cf5fa8ef4
SimovDenis <131714121+SimovDenis@users.noreply.github.com>
2023-08-01 10:08:00 +0200

    ex2

---
+++ Java/eclipse/BegnnersGuideBookExercises/src/Examples/Ex2.java
@@ -0,0 +1,18 @@
+package Examples;
+
+public class Ex2 {
+	
+	public static void main(String[] args) {
+		
+		int myVar1= 1024;
+		
+		System.out.println("myVar1 contains " + myVar1);
+		
+		int myVar2 = myVar1 / 2;
+		
+		System.out.print("myVar2 contains myVar1 / 2: ");
+		System.out.println(myVar2);
+		
+	}
+
+}
