587e9df15f5f3391263c5d6c859c59088c5e6bbd
SimovDenis <131714121+SimovDenis@users.noreply.github.com>
2023-08-02 10:31:55 +0200

    GalToLit

---
+++ Java/eclipse/BegnnersGuideBookExercises/src/TryThis/GalToLit.java
@@ -0,0 +1,19 @@
+package TryThis;
+
+public class GalToLit {
+	
+	public static void main(String[] args) {
+		
+		for (int i = 1; i <= 100; i++) {
+			
+			if (i % 10 == 0) {
+				System.out.println();
+			}
+			
+			System.out.println(i + ". " + i + " gallons equals " + i * 3.7854 + " liters");
+			
+		}
+		
+	}	 
+
+}
