9eaaa4c29f6149ad9fb9e50892b25176a74916a5
SimovDenis <131714121+SimovDenis@users.noreply.github.com>
2023-08-03 14:00:06 +0200

    MoonGravity

---
+++ Java/eclipse/BegnnersGuideBookExercises/src/TryThis/MoonGravity.java
@@ -0,0 +1,15 @@
+package TryThis;
+
+public class MoonGravity {
+	
+	public static void main(String[] args) {
+		
+//		The moon’s gravity is about 17 percent that of earth’s. Write a program that
+//		computes your effective weight on the moon.
+		
+		int myWeight = 92;
+		System.out.println("My weight on the moon is " + myWeight * 0.17);
+		
+	}
+
+}
