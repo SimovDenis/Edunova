5b1d213da6060beecd5140091497010e8e87024d
SimovDenis <131714121+SimovDenis@users.noreply.github.com>
2023-08-24 11:38:15 +0200

    NetBeansPocetak

---
+++ Java/netbeans/Ponavljanje/src/main/java/edunova/Ponavljanje.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package edunova;
+
+/**
+ *
+ * @author Denis
+ */
+public class Ponavljanje {
+
+    public static void main(String[] args) {
+        new Prozor().setVisible(true);
+    }
+}
