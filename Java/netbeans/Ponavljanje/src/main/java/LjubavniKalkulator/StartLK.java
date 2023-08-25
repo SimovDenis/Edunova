80dd7bf391f0f26fa3f88599292f98d114a9668d
SimovDenis <131714121+SimovDenis@users.noreply.github.com>
2023-08-25 10:25:39 +0200

    LjubavniJFrame

---
+++ Java/netbeans/Ponavljanje/src/main/java/LjubavniKalkulator/StartLK.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package LjubavniKalkulator;
+
+
+
+/**
+ *
+ * @author Denis
+ */
+public class StartLK {
+    
+    public static void main(String[] args) {
+        
+        new LjubavniKalkulator().setVisible(true);
+        
+    }
+    
+}
