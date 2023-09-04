c5111ee6fc854257a5a09c9142602643752af055
SimovDenis <131714121+SimovDenis@users.noreply.github.com>
2023-09-04 15:39:15 +0200

    CurrencyConverter

---
+++ Java/netbeans/CurrnecyConverter/src/main/java/currnecyconverter/Start.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package currnecyconverter;
+
+/**
+ *
+ * @author Denis
+ */
+public class Start {
+    
+    public static void main(String[] args) {
+        new MyCurrencyConverter().setVisible(true);
+    }
+    
+}
