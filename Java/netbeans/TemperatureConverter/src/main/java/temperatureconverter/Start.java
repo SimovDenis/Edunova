cfc9993d206dd06213a08b6bca08fdcc7c7b1474
SimovDenis <131714121+SimovDenis@users.noreply.github.com>
2023-09-05 11:48:27 +0200

    TemperatureConverter

---
+++ Java/netbeans/TemperatureConverter/src/main/java/temperatureconverter/Start.java
@@ -0,0 +1,12 @@
+package temperatureconverter;
+
+/**
+ *
+ * @author Denis
+ */
+public class Start {
+
+    public static void main(String[] args) {
+        new MyTempConverter().setVisible(true);
+    }
+}
