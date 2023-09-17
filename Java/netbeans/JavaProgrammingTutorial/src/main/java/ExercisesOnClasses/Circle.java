be675ce3bb8a9447b7a1f27933a24d2e7add631a
SimovDenis <131714121+SimovDenis@users.noreply.github.com>
2023-09-17 19:37:25 +0200

    JavaProgrammingTutorial-Circle

---
+++ Java/netbeans/JavaProgrammingTutorial/src/main/java/ExercisesOnClasses/Circle.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ExercisesOnClasses;
+
+/**
+ *
+ * @author Denis
+ */
+public class Circle {
+
+    private double radius = 1.0;
+    private String color = "red";
+
+    public Circle() {
+    }
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public Circle(double radius, String color) {
+        this.radius = radius;
+        this.color = color;
+    }
+
+    public double getRadius() {
+        return this.radius;
+    }
+
+    public String getColor() {
+        return this.color;
+    }
+
+    public double getArea() {
+        return this.radius * this.radius * Math.PI;
+    }
+
+}
