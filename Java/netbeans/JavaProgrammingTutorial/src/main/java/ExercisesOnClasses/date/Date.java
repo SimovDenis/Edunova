6335e2ab182680b74db6dd4a3395a3c2ded90712
SimovDenis <131714121+SimovDenis@users.noreply.github.com>
2023-09-20 11:22:44 +0200

    JavaProgrammingTutorial-DateUpdate

--- Java/netbeans/JavaProgrammingTutorial/src/main/java/ExercisesOnClasses/date/Date.java
+++ Java/netbeans/JavaProgrammingTutorial/src/main/java/ExercisesOnClasses/date/Date.java
@@ -4,70 +4,83 @@
  */
 package ExercisesOnClasses.date;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
 /**
  *
  * @author Denis
  */
 public class Date {
-    
+
     private int day;
     private int month;
     private int year;
-    
-    public Date(int day, int month, int year){
+
+    public Date(int day, int month, int year) {
         this.day = day;
         this.month = month;
         this.year = year;
     }
-    
-    public int getDay(){
+
+    public int getDay() {
         return day;
     }
-    
-    public int getMonth(){
+
+    public int getMonth() {
         return month;
     }
-    
-    public int getYear(){
+
+    public int getYear() {
         return year;
     }
-    
-    public void setDay(int day){
+
+    public void setDay(int day) {
         this.day = day;
     }
-    
-    public void setMonth(int month){
+
+    public void setMonth(int month) {
         this.month = month;
     }
-    
-    public void setYear(int year){
+
+    public void setYear(int year) {
         this.year = year;
     }
-    
-    public void setDate(int day, int month, int year){
+
+    public void setDate(int day, int month, int year) {
         this.day = day;
         this.month = month;
         this.year = year;
     }
-    
+
     @Override
-    public String toString(){
-        
-        if(day > 31 || month > 12){
-            return "Day or month not in right format";
+    public String toString() {
+
+//        if(day > 31 || month > 12){
+//            return "Day or month not in right format";
+//        }
+//        
+//        if(day < 10 && month < 10){
+//            return "0" + day + "/0" + month + "/" + year;
+//        }
+//        
+//        if(day < 10 && month >= 10){
+//            return "0" + day + "/" + month + "/" + year;
+//        }                
+//        
+//        return day + "/" + month + "/" + year;
+
+        String s = String.format("%d/%d/%d", day, month, year);
+        java.util.Date date = null;
+
+        try {
+            date = new SimpleDateFormat("dd/MM/yyyy").parse(s);
+        } catch (ParseException ex) {
+            ex.printStackTrace();
         }
-        
-        if(day < 10 && month < 10){
-            return "0" + day + "/0" + month + "/" + year;
-        }
-        
-        if(day < 10 && month >= 10){
-            return "0" + day + "/" + month + "/" + year;
-        }                
-        
-        return day + "/" + month + "/" + year;
+
+        return new SimpleDateFormat("dd/MM/yyyy").format(date);
+
     }
-    
-    
-    
+
 }
