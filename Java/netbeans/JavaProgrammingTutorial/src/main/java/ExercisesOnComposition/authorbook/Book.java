5c12ae9d1fcec8b544208a34a1b8f98392c96f10
SimovDenis <131714121+SimovDenis@users.noreply.github.com>
2023-09-22 09:18:42 +0200

    JavaProgrammingTutorial-AuthorBook

---
+++ Java/netbeans/JavaProgrammingTutorial/src/main/java/ExercisesOnComposition/authorbook/Book.java
@@ -0,0 +1,73 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ExercisesOnComposition.authorbook;
+
+/**
+ *
+ * @author Denis
+ */
+public class Book {
+
+    private String name;
+    private Author author;
+    private double price;
+    private int qty;
+
+    public Book(String name, Author author, double price) {
+        this.name = name;
+        this.author = author;
+        this.price = price;
+    }
+
+    public Book(String name, Author author, double price, int qty) {
+        this.name = name;
+        this.author = author;
+        this.price = price;
+        this.qty = qty;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getQty() {
+        return qty;
+    }
+
+    public void setQty(int qty) {
+        this.qty = qty;
+    }
+
+    @Override
+    public String toString() {
+        return "Book[name=" + name + "," + author.toString()
+                + ",price=" + price + ",qty=" + qty + "]";
+    }
+
+    public String getAuthorName() {
+        return author.getName();
+    }
+
+    public String getEmail() {
+        return author.getEmail();
+    }
+
+    public char getGender() {
+        return author.getGender();
+    }
+
+}
--- Java/netbeans/JavaProgrammingTutorial/src/main/java/testing/Test.java
+++ Java/netbeans/JavaProgrammingTutorial/src/main/java/testing/Test.java
@@ -4,7 +4,8 @@
  */
 package testing;
 
-import ExercisesOnClasses.ball.Ball;
+import ExercisesOnComposition.authorbook.Author;
+import ExercisesOnComposition.authorbook.Book;
 
 /**
  *
@@ -14,43 +15,29 @@ public class Test {
 
     public static void main(String[] args) {
 
-        Ball ball = new Ball(1.1f, 2.2f, 10, 3.3f, 4.4f);
-        System.out.println(ball);
+        Author ahTeck = new Author("Tah Ah Teck", "ahteck@nowhere.com", 'm');
+        System.out.println(ahTeck);
 
-        ball.setX(80.0f);
-        ball.setY(35.0f);
-        ball.setRadius(5);
-        ball.setxDelta(4.0f);
-        ball.setyDelta(6.0f);
-        System.out.println(ball);
+        Book dummyBook = new Book("Java for dummy", ahTeck, 19.95, 99);
+        System.out.println(dummyBook);
 
-        System.out.println("x is: " + ball.getX());
-        System.out.println("y is: " + ball.getY());
-        System.out.println("radius is: " + ball.getRadius());
-        System.out.println("xDelta is:" + ball.getxDelta());
-        System.out.println("yDelta is: " + ball.getyDelta());
+        dummyBook.setPrice(29.95);
+        dummyBook.setQty(28);
+        System.out.println("name is: " + dummyBook.getName());
+        System.out.println("price is: " + dummyBook.getPrice());
+        System.out.println("qty is: " + dummyBook.getQty());
+        System.out.println("Author is: " + dummyBook.getAuthor());
+        System.out.println("Aurhor's name is: " + dummyBook.getAuthor().getName());
+        System.out.println("Author's email is: " + dummyBook.getAuthor().getEmail());
 
-        float xMin = 0.0f;
-        float xMax = 100.0f;
-        float yMin = 0.0f;
-        float yMax = 50.0f;
+        Book anotherBook = new Book("more Java",
+                new Author("Paul Tan", "paul@somehwere.com", 'm'), 29.95);
 
-        for (int i = 0; i < 15; i++) {
-            ball.move();
-            System.out.println(ball);
+        System.out.println(anotherBook);
 
-            float xNew = ball.getX();
-            float yNew = ball.getY();
-            int radius = ball.getRadius();
-
-            if ((xNew + radius) > xMax || (xNew - radius) < xMin) {
-                ball.reflectHorizontal();
-            }
-
-            if ((yNew + radius) > yMax || (yNew - radius) < yMin) {
-                ball.reflectVertical();
-            }
-        }
+        System.out.println(anotherBook.getAuthorName());
+        System.out.println(anotherBook.getEmail());
+        System.out.println(anotherBook.getGender());
 
     }
 
