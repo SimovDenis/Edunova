76529a3f8cf7e9e1c6abd34f15008912823810eb
SimovDenis <131714121+SimovDenis@users.noreply.github.com>
2023-09-30 16:20:29 +0200

    KontrolaBrojUgovorFix

---
+++ Java/netbeans/JavaProgrammingTutorial/src/main/java/MoreExercisesOnClasses/mypolynomial/MyPolynomial.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package MoreExercisesOnClasses.mypolynomial;
+
+/**
+ *
+ * @author Denis
+ */
+public class MyPolynomial {
+    
+    private double[] coeffs;
+    
+    public MyPolynomial(double... coeffs){
+        this.coeffs = coeffs;
+    }
+    
+    
+    
+}
