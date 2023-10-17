67047dff403b7fe6a7912949396d93adc9abb1be
SimovDenis <131714121+SimovDenis@users.noreply.github.com>
2023-10-17 11:16:23 +0200

    Vjezba

--- Java/netbeans/JavaProgrammingTutorial/src/main/java/testing/Test.java
+++ Java/netbeans/JavaProgrammingTutorial/src/main/java/testing/Test.java
@@ -1,44 +1,26 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package testing;
 
-import MoreExercisesOnClasses.mycomplex.MyComplex;
-import MoreExercisesOnClasses.mycomplex.MyComplexApp;
+import com.github.javafaker.Faker;
 
-/**
- *
- * @author Denis
- */
+import java.time.Instant;
+import java.time.LocalTime;
+import java.time.ZoneId;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 public class Test {
 
     public static void main(String[] args) {
+        Faker faker = new Faker();        
 
-//        MyComplex c1 = new MyComplex(3.4, 0);
-//        MyComplex c2 = new MyComplex(2.8, 1.3);
-//        System.out.println(c1);
-//        System.out.println(c1.isReal());
-//        System.out.println(c1.isImaginary());
-//        c1.setImag(2.2);
-//        System.out.println(c1.isReal());
-//
-//        System.out.println(c1.equals(3.4, 2.2));
-//
-//        System.out.println(c1.magnitude());
-//        System.out.println(c1);
-//
-//        MyComplex c3 = c1.addNew(c2);
-//        MyComplex c4 = c1.addInto(c2);
-//
-//        System.out.println(c4);
-//        System.out.println(c1);
-//        System.out.println(c3);
+        List<LocalTime> lista = new ArrayList<>();
 
-
-          new MyComplexApp();
-
-
+        for (int i = 0; i < 5; i++) {
+            Date vrijeme = faker.date().birthday();
+            LocalTime satMin = Instant.ofEpochMilli(vrijeme.getTime()).atZone(ZoneId.systemDefault()).toLocalTime();            
+            lista.add(satMin);
+            System.out.println(lista.get(i));
+        }
     }
-
 }
